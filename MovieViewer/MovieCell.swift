1bf3f79c76d5283669f689bbf41f7d9f83fe967d
shakeeld <shakeeldaswani@gmail.com>
2016-02-07 23:15:37 -0500

    Added the poster images, the pull to refresh, and the loading state

--- MovieViewer/MovieCell.swift
+++ MovieViewer/MovieCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class MovieCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
 
+    @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var overviewLabel: UILabel!
     
     override func awakeFromNib() {
--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var TableView: UITableView!
     var movies: [NSDictionary]?
+   // let data = []
+   // var filteredData: [String]!
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        TableView.dataSource = self
-        TableView.delegate = self
-        
-        
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
         
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
@@ -34,6 +35,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue: NSOperationQueue.mainQueue()
         )
         
+        
+        
         let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
                 if let data = dataOrNil {
@@ -43,6 +46,62 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             
                             self.movies = responseDictionary["results"] as! [NSDictionary]
                             self.TableView.reloadData()
+                            refreshControl.endRefreshing()
+                            
+                    }
+                }
+        })
+        task.resume()
+        
+    }
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        TableView.insertSubview(refreshControl, atIndex: 0)
+        
+        TableView.dataSource = self
+        TableView.delegate = self
+        //searchBar.delegate = self
+        //filteredData = data as! [String]
+        
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        
+            
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            
+                            
+                            
+                            
+                            self.TableView.reloadData()
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+                            
+                            
                     }
                 }
         })
@@ -59,13 +118,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let movies = movies {
             return movies.count
+            //return filteredData.count
         }
         else {
          return 0
             }
+/*
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }  */
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -75,17 +140,34 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        
-        
+    
+        //cell.textLabel?.text = filteredData[indexPath.row]
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        let posterPath = movie["poster_path"] as! String
+        let baseUrl = "http://image.tmdb.org/t/p/w500"
+        let imageUrl = NSURL(string: baseUrl + posterPath)
         
-        
+        cell.posterView.setImageWithURL(imageUrl!)
         
         print("row \(indexPath.row)")
         return cell
         
     }
+    
+   /* func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? data : data.filter({(dataString: String) -> Bool in
+            return dataString.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+        
+    }
+
+*/
+    
+        
+    
+    
+    
     
     
     
@@ -99,5 +181,4 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Pass the selected object to the new view controller.
     }
     */
-
 }
